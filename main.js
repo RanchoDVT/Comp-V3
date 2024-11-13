fba68b4a5c905dd829120ebf495dbdeddfe12e60
Voidless7125 <167136334+Voidless7125@users.noreply.github.com>
2024-11-13 20:42:33 +0000

    0.8.4 Color optimization, fix links and program misnames

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const copyButton = document.getElementById('copy-button');
     const configOutput = document.getElementById('config-output');
     marked.use({ breaks: true });
-    
+
     const fetchFile = async (url, targetElement) => {
         try {
             const response = await fetch(url);
@@ -30,8 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
 
-    if (readmeContent) fetchFile('https://raw.githubusercontent.com/RanchoDVT/Comp-V5/dev/README.md', readmeContent);
-    if (changelogContent) fetchFile('https://raw.githubusercontent.com/RanchoDVT/Comp-V5/dev/changelog.md', changelogContent);
+    if (readmeContent) fetchFile('https://raw.githubusercontent.com/RanchoDVT/Comp-V3/dev/README.md', readmeContent);
+    if (changelogContent) fetchFile('https://raw.githubusercontent.com/RanchoDVT/Comp-V3/dev/changelog.md', changelogContent);
 
     const getLatestRelease = async (repo) => {
         try {
@@ -59,13 +59,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     REAR_RIGHT_MOTOR { PORT=${formData.get('rear_right_port')} GEAR_RATIO=${formData.get('rear_right_gear_ratio')} REVERSED=${formData.has('rear_right_reversed')} }
                     INERTIAL { PORT=${formData.get('inertial_port')} }
                     Rear_Bumper { PORT=${formData.get('rear_bumper_port')} }
-                    PRINTLOGO=${formData.has('print_logo')}
-                    LOGTOFILE=${formData.has('log_to_file')}
-                    MAXOPTIONSSIZE=${formData.get('max_options_size')}
-                    POLLINGRATE=${formData.get('polling_rate')}
-                    CTRLR1POLLINGRATE=${formData.get('ctrlr1_polling_rate')}
-                    VERSION=${await getLatestRelease('RanchoDVT/Comp-V5')}
-                }`;
+
+                }
+                PRINTLOGO=${formData.has('print_logo')}
+                LOGTOFILE=${formData.has('log_to_file')}
+                MAXOPTIONSSIZE=${formData.get('max_options_size')}
+                POLLINGRATE=${formData.get('polling_rate')}
+                CTRLR1POLLINGRATE=${formData.get('ctrlr1_polling_rate')}
+                VERSION=${await getLatestRelease('RanchoDVT/Comp-V3')}`;
 
             copyButton.style.display = 'inline-block';
         });
@@ -88,18 +89,18 @@ document.addEventListener('DOMContentLoaded', () => {
         let popupText = '', downloadLink = '';
 
         if (type === 'dev') {
-            popupText = 'Thank you for downloading Comp_V3 *dev*! This download link goes to the Github API. This is the source code. <br> <br> You will need my Custom SDK to use this. Check out my other download in the navbar.';
-            downloadLink = 'https://github.com/RanchoDVT/Comp-V5/archive/refs/heads/dev.zip';
+            popupText = 'Thank you for downloading Comp-V3 *dev*! This download link goes to the Github API. This is the source code. <br> <br> You will need my Custom SDK to use this. Check out my other download in the navbar.';
+            downloadLink = 'https://github.com/RanchoDVT/Comp-V3/archive/refs/heads/dev.zip';
             document.getElementById('popup-title').innerText = 'Download Comp-V3 ' + type;
         } else if (type === 'stable') {
-            const latestTag = await getLatestRelease('RanchoDVT/Comp-V5');
-            popupText = 'Thank you for downloading Comp_V3 stable! This download link goes to the Github API. This is the source code. <br> <br> You will need my Custom SDK to use this. Check out my other download in the navbar.';
-            downloadLink = `https://github.com/RanchoDVT/Comp-V5/archive/refs/tags/${latestTag}.zip`;
+            const latestTag = await getLatestRelease('RanchoDVT/Comp-V3');
+            popupText = 'Thank you for downloading Comp-V3 stable! This download link goes to the Github API. This is the source code. <br> <br> You will need my Custom SDK to use this. Check out my other download in the navbar.';
+            downloadLink = `https://github.com/RanchoDVT/Comp-V3/archive/refs/tags/${latestTag}.zip`;
             document.getElementById('popup-title').innerText = 'Download Comp-V3 ' + type + ' ' + latestTag;
         } else if (type === 'sdk') {
             const latestTag = await getLatestRelease('RanchoDVT/Vex-SDK');
-            popupText = 'Thank you for downloading my custom SDK. This is unofficial and in no way affiliated, endorsed, supported, or created by VEX Robotics. <br> <br> You will need this to install my Custom SDK (This) to use my Comp_V3 Program. This modifies Vex\'s robotics extension, so PLEASE don\'t go to them if you have problems with this. Please contact me. <br> <br>There is a PowerShell script for this to make it easier: ';
-            popupText += '<a href="https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/RanchoDVT/Vex-SDK/blob/dev/Vex-SDK.updater.ps1">Powershell download</a>';
+            popupText = 'Thank you for downloading my custom SDK. This is unofficial and in no way affiliated, endorsed, supported, or created by VEX Robotics. <br> <br> You will need this to install my Custom SDK (This) to use my Comp-V3 Program. This modifies Vex\'s robotics extension, so PLEASE don\'t go to them if you have problems with this. Please contact me. <br> <br>There is a PowerShell script for this to make it easier: ';
+            popupText += '<a target="_blank" href="https://github.com/RanchoDVT/Vex-SDK/blob/dev/Vex-SDK.updater.ps1">Powershell download</a>';
             document.getElementById('popup-title').innerText = 'Download Custom ' + type + ' ' + latestTag;
             downloadLink = `https://github.com/RanchoDVT/Vex-SDK/archive/refs/tags/${latestTag}.zip`;
         }
